41150a0a02dafab86f6831c40ff2624a571861c4
Kamesh <2300031070@kluniversity.in>
2025-10-18 10:26:08 +0530

    frontend updated

--- frontend-reactapp/src/App.jsx
+++ frontend-reactapp/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
+//import Navbar from "./components/Navbar";
 
 function App() {
   return (
